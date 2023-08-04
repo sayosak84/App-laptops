0087860eea0f632ecd7f0124bdd22b231809396f
SASSI Nader <nader_84@hotmail.fr>
2023-08-04 19:19:05 +0200

    add app.py 2 fnct succes

--- app.py
+++ app.py
@@ -3,12 +3,30 @@ import tkinter as tk
 from tkinter import ttk
 import tkinter.font as font
 from PIL import Image, ImageTk
+import sqlite3
+import os
 
 IMAGE_PATH_FIRST = 'C:\\Users\\Nader\\Documents\\projets_lotfi\\App-laptops\\first-page.jpg'
 IMAGE_PATH_OTHERS = 'C:\\Users\\Nader\\Documents\\projets_lotfi\\App-laptops\\other-pages.jpg'
 BUTTON_FONT = None
 WIDTH = 1200
 HEIGTH = 800
+LARGEFONT =("Verdana", 35)
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+conn = sqlite3.connect(resource_path('test.db'))
+
+
 
 class BkgrFrame(tk.Frame):
     def __init__(self, parent, file_path, width, height):
@@ -190,69 +208,51 @@ class Ajouter(tk.Frame):
         bkrgframe = BkgrFrame(self, IMAGE_PATH_OTHERS, WIDTH, HEIGTH)
         bkrgframe.pack()
 
-            # create a Form label
-        #heading = Label(self, text="Form", bg="light green")
-
-
-
-        heading = bkrgframe.add(Label(self, text="Form", bg="#DEC6FA")
-                            , 200, 210)
+        Titre = bkrgframe.add(Label(self, text ="Gestion des laptops", bg="#DEC6FA", font = LARGEFONT)
+                            , 300, 100)
 
         nom_appareil = bkrgframe.add(Label(self, text="Nom de l'appareil", bg="#DEC6FA")
                             , 200, 240)
 
-            # create a marque label
-        marque = Label(self, text="Marque", bg="light green")
+        marque = bkrgframe.add(Label(self, text="Marque", bg="#DEC6FA")
+                            , 200, 260)
 
-            # create a Date de l'achat label
-        date_achat = Label(self, text="Date de l'achat", bg="light green")
+        date_achat = bkrgframe.add(Label(self, text="Date de l'achat", bg="#DEC6FA")
+                            , 200, 280)
 
-            # create a date_exp label
-        date_exp = Label(self, text="Date d'expiration", bg="light green")
+        date_exp = bkrgframe.add(Label(self, text="Date d'expiration", bg="#DEC6FA")
+                            , 200, 300)
 
-            # create a prix_achat label
-        prix_achat = Label(self, text="Prix d'achat", bg="light green")
+        prix_achat = bkrgframe.add(Label(self, text="Prix d'achat", bg="#DEC6FA")
+                            , 200, 320)
 
-            # create a Email id label
-        date_compta = Label(self, text="Date comptabilisation", bg="light green")
+        date_compta = bkrgframe.add(Label(self, text="Date comptabilisation", bg="#DEC6FA")
+                            , 200, 340)
 
-            # create a ammort_mensuel label
-        ammort_mensuel = Label(self, text="Ammortissement mensuel", bg="light green")
+        ammort_mensuel = bkrgframe.add(Label(self, text="Ammortissement mensuel", bg="#DEC6FA")
+                            , 200, 360)
 
-            # create a utilisateur label
-        utilisateur = Label(self, text="Utilisateur", bg="light green")
+        utilisateur = bkrgframe.add(Label(self, text="Utilisateur", bg="#DEC6FA")
+                            , 200, 380)
 
-            # create a date affectation label
-        date_affect = Label(self, text="Date affectation", bg="light green")
+        date_affect = bkrgframe.add(Label(self, text="Date affectation", bg="#DEC6FA")
+                            , 200, 400)
 
-            # grid method is used for placing
-            # the widgets at respective positions
-            # in table like structure .
         
-        '''
-        heading.grid(row=0, column=1)
-        nom_appareil.grid(row=1, column=0)
-        marque.grid(row=2, column=0)
-        date_achat.grid(row=3, column=0)
-        date_exp.grid(row=4, column=0)
-        prix_achat.grid(row=5, column=0)
-        date_compta.grid(row=6, column=0)
-        ammort_mensuel.grid(row=7, column=0)
-        utilisateur.grid(row=8, column=0)
-        date_affect.grid(row=9, column=0)
 
         # create a text entry box
         # for typing the information
-        nom_appareil_field = Entry(self)
-        marque_field = Entry(self)
-        date_achat_field = Entry(self)
-        date_exp_field = Entry(self)
-        prix_achat_field = Entry(self)
-        date_compta_field = Entry(self)
-        ammort_mensuel_field = Entry(self)
-        utilisateur_field = Entry(self)
-        date_affect_field = Entry(self)
+        nom_appareil_field = bkrgframe.add(Entry(self, width= 40), 400, 240)
+        marque_field = bkrgframe.add(Entry(self, width= 40), 400, 260)
+        date_achat_field = bkrgframe.add(Entry(self, width= 40), 400, 280)
+        date_exp_field = bkrgframe.add(Entry(self, width= 40), 400, 300)
+        prix_achat_field = bkrgframe.add(Entry(self, width= 40), 400, 320)
+        date_compta_field = bkrgframe.add(Entry(self, width= 40), 400, 340)
+        ammort_mensuel_field = bkrgframe.add(Entry(self, width= 40), 400, 360)
+        utilisateur_field = bkrgframe.add(Entry(self, width= 40), 400, 380)
+        date_affect_field = bkrgframe.add(Entry(self, width= 40), 400, 400)
 
+        
         # bind method of widget is used for
         # the binding the function with the events
 
@@ -284,34 +284,30 @@ class Ajouter(tk.Frame):
 
         utilisateur_field.bind("<Return>", focus8)
 
+        submit = bkrgframe.add(tk.Button(self, 
+                            text ="Ajouter",
+                            command=insert,
+                            bg='#45b592',
+                            fg='#ffffff',
+                            bd=0,
+                            font=BUTTON_FONT,
+                            height=2,
+                            width=15
+                            )
+                            , 530, 430)
 
-
-        # grid method is used for placing
-        # the widgets at respective positions
-        # in table like structure .
-        nom_appareil_field.grid(row=1, column=1, ipadx="100")
-        marque_field.grid(row=2, column=1, ipadx="100")
-        date_achat_field.grid(row=3, column=1, ipadx="100")
-        date_exp_field.grid(row=4, column=1, ipadx="100")
-        prix_achat_field.grid(row=5, column=1, ipadx="100")
-        date_compta_field.grid(row=6, column=1, ipadx="100")
-        ammort_mensuel_field.grid(row=7, column=1, ipadx="100")
-        utilisateur_field.grid(row=8, column=1, ipadx="100")
-        date_affect_field.grid(row=9, column=1, ipadx="100")
-
-        # create a Submit Button and place into the self window
-        submit = Button(self, text="Submit", fg="Black",
-                                bg="Red", command=insert)
-        submit.grid(row=10, column=1)
-
-
-        button1 = ttk.Button(self, text ="Retour",
-                                command = lambda : controller.show_frame(Page1))
-     
-        # putting the button in its place
-        # by using grid
-        button1.grid(row = 11, column = 1, padx = 10, pady = 10)
-        '''
+        button1 = bkrgframe.add(tk.Button(self, 
+                            text ="Retour",
+                            command = lambda : controller.show_frame(Acceuil),
+                            bg='#FFCE5F',
+                            fg='#ffffff',
+                            bd=0,
+                            font=BUTTON_FONT,
+                            height=2,
+                            width=15
+                            )
+                            , 700, 550)
+        
 
 # second window frame Ajouter
 class Acceuil(tk.Frame):
@@ -335,32 +331,77 @@ class Acceuil(tk.Frame):
                             font=BUTTON_FONT,
                             height=2,
                             width=15
-                            ), 400, 400)
+                            ), 550, 400)
 
         button1 = bkrgframe.add(tk.Button(self, 
-                            text ="Rechercher",
+                            text ="Rechercher par nom d'appareil",
                             command = lambda : controller.show_frame(Rechercher_laptop),
                             bg='#45b592',
                             fg='#ffffff',
                             bd=0,
                             font=BUTTON_FONT,
                             height=2,
-                            width=15
-                            ), 800, 400)
+                            width=30
+                            ), 500, 450)
 
-# second window frame Ajouter
+# second window frame Rechercher_laptop
 class Rechercher_laptop(tk.Frame):
      
     def __init__(self, parent, controller):
+        
+        def change_text(entry,txt):
+            entry.delete(0,END)
+            entry.insert(0,txt)
+
+        def clear_text(entry):
+            entry.delete(0,END)
+
+        def recherche(name):
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM laptops where Nom_appareil=?", (name,))
+            row = cursor.fetchone()
+
+            if row :
+                change_text(nom_appareil_field,row[0])
+                change_text(marque_field,row[1])
+                change_text(date_achat_field,row[2])
+                change_text(date_exp_field,row[3])
+                change_text(prix_achat_field,row[4])
+                change_text(date_compta_field,row[5])
+                change_text(ammort_mensuel_field,row[6])
+                change_text(utilisateur_field,row[7])
+                change_text(date_affect_field,row[8])
+            else :
+                clear_text(nom_appareil_field)
+                clear_text(marque_field)
+                clear_text(date_achat_field)
+                clear_text(date_exp_field)
+                clear_text(prix_achat_field)
+                clear_text(date_compta_field)
+                clear_text(ammort_mensuel_field)
+                clear_text(utilisateur_field)
+                clear_text(date_affect_field)
+
+                
 
         tk.Frame.__init__(self, parent)
 
         bkrgframe = BkgrFrame(self, IMAGE_PATH_OTHERS, WIDTH, HEIGTH)
         bkrgframe.pack()
 
-        button1 = bkrgframe.add(tk.Button(self, 
-                            text ="Retour",
-                            command = lambda : controller.show_frame(Ajouter),
+        Titre = bkrgframe.add(Label(self, text ="Rechercher un laptop", bg="#DEC6FA", font = LARGEFONT)
+                            , 300, 100)
+        
+        # recherche section
+
+        nom_appareil_recherche = bkrgframe.add(Label(self, text="Tapez le nom de l'appareil :", bg="#DEC6FA")
+                            , 300, 200)
+
+        nom_appareil_recherche_field = bkrgframe.add(Entry(self), 500, 200)
+
+        Recherchebtn = bkrgframe.add(tk.Button(self, 
+                            text ="Rechercher",
+                            command = lambda : recherche(nom_appareil_recherche_field.get()),
                             bg='#45b592',
                             fg='#ffffff',
                             bd=0,
@@ -368,7 +409,66 @@ class Rechercher_laptop(tk.Frame):
                             height=2,
                             width=15
                             )
-                            , 20, 10)
+                            , 700, 190)
+
+
+        # affichage section
+
+        nom_appareil = bkrgframe.add(Label(self, text="Nom de l'appareil", bg="#DEC6FA")
+                            , 300, 280)
+
+        marque = bkrgframe.add(Label(self, text="Marque", bg="#DEC6FA")
+                            , 300, 300)
+
+        date_achat = bkrgframe.add(Label(self, text="Date de l'achat", bg="#DEC6FA")
+                            , 300, 320)
+
+        date_exp = bkrgframe.add(Label(self, text="Date d'expiration", bg="#DEC6FA")
+                            , 300, 340)
+
+        prix_achat = bkrgframe.add(Label(self, text="Prix d'achat", bg="#DEC6FA")
+                            , 300, 360)
+
+        date_compta = bkrgframe.add(Label(self, text="Date comptabilisation", bg="#DEC6FA")
+                            , 300, 380)
+
+        ammort_mensuel = bkrgframe.add(Label(self, text="Ammortissement mensuel", bg="#DEC6FA")
+                            , 300, 400)
+
+        utilisateur = bkrgframe.add(Label(self, text="Utilisateur", bg="#DEC6FA")
+                            , 300, 420)
+
+        date_affect = bkrgframe.add(Label(self, text="Date affectation", bg="#DEC6FA")
+                            , 300, 440)
+
+        
+
+        # create a text entry box
+        # for typing the information
+        nom_appareil_field = bkrgframe.add(Entry(self), 450, 280)
+        marque_field = bkrgframe.add(Entry(self), 450, 300)
+        date_achat_field = bkrgframe.add(Entry(self), 450, 320)
+        date_exp_field = bkrgframe.add(Entry(self), 450, 340)
+        prix_achat_field = bkrgframe.add(Entry(self), 450, 360)
+        date_compta_field = bkrgframe.add(Entry(self), 450, 380)
+        ammort_mensuel_field = bkrgframe.add(Entry(self), 450, 400)
+        utilisateur_field = bkrgframe.add(Entry(self), 450, 420)
+        date_affect_field = bkrgframe.add(Entry(self), 450, 440)
+
+
+        
+
+        Retour = bkrgframe.add(tk.Button(self, 
+                            text ="Retour",
+                            command = lambda : controller.show_frame(Acceuil),
+                            bg='#FFCE5F',
+                            fg='#ffffff',
+                            bd=0,
+                            font=BUTTON_FONT,
+                            height=2,
+                            width=15
+                            )
+                            , 700, 550)
 
 
     
